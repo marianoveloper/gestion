d2ab0e4c90835d9d88862766b3b8fc62b1470485
Mariano <benitezmariano5@gmail.com>
2021-11-02 02:17:48 -0300

    mail

--- app/Http/Controllers/ConsultaController.php
+++ app/Http/Controllers/ConsultaController.php
@@ -15,7 +15,7 @@ class ConsultaController extends Controller
 
     public function store(Request $request){
         $correo = new Consulta($request->all());
-        Mail::to('toto@gmail.com')->send($correo);
+        Mail::to('soportevirtual@uccuyo.edu.ar')->send($correo);
 
         return redirect()->route('consulta.index')->with('info', 'Su consulta ha sido enviada con exito.');
     }

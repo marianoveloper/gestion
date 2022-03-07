84c35aac04ffddd84e8243e39ddb0127fa1147cc
Mariano <benitezmariano5@gmail.com>
2022-03-07 20:48:05 -0300

    cart

--- resources/views/components/course-filter.blade.php
+++ resources/views/components/course-filter.blade.php
@@ -7,7 +7,7 @@
             </figure>
             <div class="px-6 py-4">
 
-                <a target="_blank" href="{{$course->link_form}}"
+                <a  href="{{$course->link_form}}"
                     class="block px-4 py-2 mt-4 font-bold text-center text-white bg-green-600 rounded hover:bg-green-800">
                     Ingresar
                 </a>

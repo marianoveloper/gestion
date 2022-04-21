54946c3cef406239695aaf908b93f18dc59ad4ac
Mariano <benitezmariano5@gmail.com>
2022-04-21 19:17:08 -0300

    actualizacion

---
+++ database/migrations/2022_04_21_183723_create_materias_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMateriasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('materias', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('year');
+            $table->string('code');
+
+            $table->unsignedBigInteger('carreras_id')->nullable();
+            $table->foreign('carreras_id')->references('id')->on('carreras')->onDelete('set null');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('materias');
+    }
+}

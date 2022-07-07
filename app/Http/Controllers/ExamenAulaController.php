438e89bc21767f0e5299a6c25e2372a59ff96835
Mariano <benitezmariano5@gmail.com>
2022-07-07 20:51:56 -0300

    examen

---
+++ app/Http/Controllers/ExamenAulaController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+use Illuminate\Http\Request;
+
+class ExamenAulaController extends Controller
+{
+    public function index(){
+
+
+        return view('examen.aula.index');
+
+
+    }
+}

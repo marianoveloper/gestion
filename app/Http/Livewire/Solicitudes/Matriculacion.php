3ae862fb75ef7a507dbdf251919d030e94775bfd
Mariano <benitezmariano5@gmail.com>
2023-08-03 19:57:43 -0300

    soli funciona

---
+++ app/Http/Livewire/Solicitudes/Matriculacion.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Solicitudes;
+
+use Livewire\Component;
+
+class Matriculacion extends Component
+{
+    public function render()
+    {
+        return view('livewire.solicitudes.matriculacion');
+    }
+}

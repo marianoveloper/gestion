eeca6b13c44f90ea672fc9eb10bedde13e2b5768
Mariano Benitez <benitezmariano5@gmail.com>
2024-03-07 19:45:54 -0300

    homecontroller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -13,33 +13,23 @@ class HomeController extends Controller
     public function __invoke()
     {
         $usu=auth()->user()->id;
-        if($usu<54){
-           $valor=1;
-        }
-        else{
-            $valor=2;
-        }
-        switch($valor){
-            
-           
-            case 1:
+
+
+
+
             $courses=Course::where('status',1)
 
             ->latest('id')->get()->take(8);
             $types= Type::all();
             $category=Category::all();
-    
-            
+
+
            return view('welcome',compact('courses','types','category'));
-                break;
-                case 2:
-                    return view('sala.candidato.index');
-                    break;
-            
-            }
 
 
 
-      
+
+
+
     }
 }
